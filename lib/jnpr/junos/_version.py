91f5e95a80f812aa0fac8fbad106c57c5837bc7a
Jad Haj Yahya <jhajyahy@redhat.com>
2024-01-06 08:02:38 +0200

    fix

--- lib/jnpr/junos/_version.py
+++ lib/jnpr/junos/_version.py
@@ -52,7 +52,7 @@ def get_config() -> VersioneerConfig:
     cfg.VCS = "git"
     cfg.style = "pep440"
     cfg.tag_prefix = ""
-    cfg.parentdir_prefix = "None"
+    cfg.parentdir_prefix = ""
     cfg.versionfile_source = "lib/jnpr/junos/_version.py"
     cfg.verbose = False
     return cfg
